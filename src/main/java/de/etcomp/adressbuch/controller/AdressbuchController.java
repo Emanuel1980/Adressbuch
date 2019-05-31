b056a1c7cfe5e724f1cadb3f3c089e6c21920ce2
unknown <Emanuel.Tebbe@gmail.com>
2019-05-31 14:57:00 +0200

    add params to sayhello

--- src/main/java/de/etcomp/adressbuch/controller/AdressbuchController.java
+++ src/main/java/de/etcomp/adressbuch/controller/AdressbuchController.java
@@ -1,14 +1,17 @@
 package de.etcomp.adressbuch.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdressbuchController {
 	
+	//URL-Example = http://localhost:8080/hello?vorname=Emanuel&nachname=Tebbe
 	@RequestMapping(path="/hello")
-	public String sayHello() {
-		return "Hello";
+	public String sayHello(@RequestParam(value="nachname", defaultValue="Nachname") String nachname,
+			@RequestParam(value="vorname", defaultValue="Vorname") String vorname) {
+		return String.format("Hallo %s %s", vorname, nachname);
 	}
 
 }
